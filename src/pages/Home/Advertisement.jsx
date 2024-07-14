76667e5f1a09383c6c5cc47d8f78d399bf251f6c
bjoarder7 <bjoarder87@gmail.com>
2024-07-14 23:28:29 +0600

    slider & animation section done

--- src/pages/Home/Advertisement.jsx
+++ src/pages/Home/Advertisement.jsx
@@ -1,11 +1,39 @@
-
-
 const Advertisement = () => {
-    return (
-        <div>
-            <h2>add</h2>
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-min">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-xl text-rose-600 font-bold"> UPTO 40% OFF LIST</h1>
+            <p className="py-2">
+            Art & craft <span className="text-red-600">OFFER</span> price.
+            </p>
+            <button className="btn btn-sm">Shop Now</button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="hero bg-base-200 min-h-min">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-xl text-rose-600 font-bold"> UPTO 40% OFF LIST</h1>
+            <p className="py-2">
+            
+            Art & craft <span className="text-rose-600">OFFER</span> price.
+            </p>
+            <button className="btn btn-sm">Shop Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Advertisement;
--- src/pages/Home/Slider.jsx
+++ src/pages/Home/Slider.jsx
@@ -1,10 +1,19 @@
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import slider1 from "../../assets/slider1.jpg";
+import slider2 from "../../assets/slider2.jpg";
+import slider3 from "../../assets/slider3.png";
 
 
-const Slider = () => {
+const Slider = () => {  
+
+      
     return (
-        <div>
-            <h2>slider</h2>
-        </div>
+        <AwesomeSlider animation="cubeAnimation">
+        <div data-src={slider1} />
+        <div data-src={slider2} />
+        <div data-src={slider3} />
+      </AwesomeSlider>
     );
 };
 
